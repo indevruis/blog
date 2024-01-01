5a0a7c7220f74cd2a43ddf789a8f79b1c2e2371f
ruirun <indevruis@gmail.com>
2024-01-01 14:04:12 +0900

    Initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "indevruis blog",
+  description: "next.js + typescript + tailwindcss blog starter",
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;

f835d8185b0e2fb7a85b46398f07d8ac05de0707
ruirun <indevruis@gmail.com>
2024-01-13 00:23:40 +0900

    Feat: get now post

--- src/app/note/[...post]/page.tsx
+++ src/app/note/[...post]/page.tsx
@@ -1,16 +1,38 @@
-'use client'
+import dayjs from 'dayjs'
+import { allPosts } from '@/contentlayer/generated'
+import { notFound } from 'next/navigation'
 
-import { NotePostAtom } from '@/states/NotePostAtom'
-import { useRecoilValue } from 'recoil'
+interface Params {
+  params: {
+    post: string[]
+  }
+}
 
-const NotePost = () => {
-  const notePost = useRecoilValue(NotePostAtom)
+const NotePost = async ({ params }: Params) => {
+  const post = await getNowPost({ params })
+
+  if (!post) {
+    notFound()
+  }
+
   return (
     <div>
-      <h1>{notePost.title}</h1>
-      <p>{notePost.description}</p>
+      <h3 className="text-[30px]">{post!.title}</h3>
+      <time>{dayjs(post!.createdAt).format('YYYY.MM.DD')}</time>
+      <div>{post!.hashtage}</div>
+      <div>{post!.thumbnail}</div>
+      <div dangerouslySetInnerHTML={{ __html: post!.body.html }}></div>
     </div>
   )
 }
 
 export default NotePost
+
+const getNowPost = async ({ params }: Params) => {
+  const nowParams = Object.values(params.post).join('/')
+  const post = allPosts.find((params) => {
+    return params._raw.flattenedPath === nowParams
+  })
+
+  return post
+}

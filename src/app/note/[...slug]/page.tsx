c7638aa61a8da360ed8ddfdd101cfdaad9d5d327
ruirun <indevruis@gmail.com>
2024-01-14 00:13:27 +0900

    Fix dangerouslySetInnerHTML -> MDXRemote

--- src/app/note/[...slug]/page.tsx
+++ src/app/note/[...slug]/page.tsx
@@ -1,38 +1,37 @@
 import dayjs from 'dayjs'
 import { allPosts } from '@/contentlayer/generated'
 import { notFound } from 'next/navigation'
+import { MDXRemote } from 'next-mdx-remote/rsc'
 
 interface Params {
   params: {
-    post: string[]
+    slug: string
   }
 }
 
 const NotePost = async ({ params }: Params) => {
   const post = await getNowPost({ params })
-
   if (!post) {
     notFound()
   }
-
   return (
     <div>
-      <h3 className="text-[30px]">{post!.title}</h3>
+      <h1 className="text-30px">{post!.title}</h1>
       <time>{dayjs(post!.createdAt).format('YYYY.MM.DD')}</time>
-      <div>{post!.hashtage}</div>
       <div>{post!.thumbnail}</div>
-      <div dangerouslySetInnerHTML={{ __html: post!.body.html }}></div>
+      <div className='.MDX'>
+      <MDXRemote source={post.body.html}/>
+      </div>
     </div>
   )
 }
 
 export default NotePost
 
-const getNowPost = async ({ params }: Params) => {
-  const nowParams = Object.values(params.post).join('/')
-  const post = allPosts.find((params) => {
-    return params._raw.flattenedPath === nowParams
+export const getNowPost = async ({ params }: Params) => {
+  const post = allPosts.find((post) => {
+    return params.slug[0] === post.slug.substring(12)
   })
-
-  return post
+  
+  return post ?? null
 }

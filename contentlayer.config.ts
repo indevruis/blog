33094f7cb076037740de32024c9d4a2bd0393d0d
ruirun <indevruis@gmail.com>
2024-01-17 16:31:18 +0900

    Feat: contentType md -> mdx

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -7,6 +7,7 @@ import remarkGfm from 'remark-gfm'
 export const Post = defineDocumentType(() => ({
   name: 'Post',
   filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
   fields: {
     title: {
       type: 'string',

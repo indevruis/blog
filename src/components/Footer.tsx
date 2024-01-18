e018190ff4624568a6bc0bdda6cf4fdbaf5c0f10
ruirun <indevruis@gmail.com>
2024-01-19 00:13:50 +0900

    Feat: images -> react icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,32 +1,27 @@
-import Image from 'next/image'
 import Link from 'next/link'
+import { TfiEmail } from 'react-icons/tfi'
+import { FaGithub } from 'react-icons/fa'
+import { SlSocialLinkedin } from 'react-icons/sl'
 
 const Footer = () => {
   return (
     <footer>
       <div className="mt-16 mb-5">
         <div className="flex justify-center">
-          <Link href="/" className="px-2">
-            <Image src="/images/email.png" width={20} height={20} alt="Email" />
+          <Link href="mailto:indevruis@gmail.com" className="px-1.5">
+            <TfiEmail color="#64748b" />
           </Link>
-          <Link href="/" className="px-2">
-            <Image
-              src="/images/github.png"
-              width={20}
-              height={20}
-              alt="Email"
-            />
+          <Link href="https://github.com/indevruis" className="px-1.5">
+            <FaGithub color="#64748b" />
           </Link>
-          <Link href="/" className="px-2">
-            <Image
-              src="/images/linkedIn.png"
-              width={20}
-              height={20}
-              alt="Email"
-            />
+          <Link
+            href="https://www.linkedin.com/in/%EC%84%9C%EC%9C%A4-%EC%B5%9C-1208312a4/"
+            className="px-1.5"
+          >
+            <SlSocialLinkedin color="#64748b" />
           </Link>
         </div>
-        <div className="flex justify-center mt-1">
+        <div className="flex justify-center mt-0.5 text-[13px] text-slate-500">
           <div>© 2024</div>
           <div>&nbsp; • &nbsp;</div>
           <div>SeoYun Choi</div>

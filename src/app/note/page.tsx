3678859d71936f5a8bb5978cd2d4f5de895a8e81
ruirun <indevruis@gmail.com>
2024-01-20 15:11:15 +0900

    Fix: get note data

--- src/app/note/page.tsx
+++ src/app/note/page.tsx
@@ -5,13 +5,14 @@ import { compareDesc } from 'date-fns'
 
 type PostsByYear = Record<string, Post[]>
 
-const Note = () => {
-  const posts = allPosts.sort(
-    (a: Post, b: Post) =>
-    compareDesc(new Date(a.createdAt), new Date(b.createdAt)))
+const Note = async () => {
+  const posts = await getPost('note')
+  const notePost = posts.sort((a: Post, b: Post) =>
+    compareDesc(new Date(a.createdAt), new Date(b.createdAt)),
+  )
   const postsByYear: PostsByYear = {}
-  posts.forEach((post) => {
-    const year = post.createdAt.substring(0,4)
+  notePost.forEach((post) => {
+    const year = post.createdAt.substring(0, 4)
     if (!postsByYear[year]) {
       postsByYear[year] = []
     }
@@ -27,9 +28,9 @@ const Note = () => {
             <div className="flex border-t py-1" key={i}>
               <p className="mr-10 py-3 text-[14px]">{year}</p>
               <div className="flex flex-col w-full">
-                {posts.map(
+                {notePost.map(
                   (post, i) =>
-                    year === post.createdAt.substring(0,4) && (
+                    year === post.createdAt.substring(0, 4) && (
                       <PostList post={post} key={i} />
                     ),
                 )}
@@ -42,3 +43,11 @@ const Note = () => {
 }
 
 export default Note
+
+export const getPost = async (dir: string) => {
+  const post = allPosts.filter((post) => {
+    return post._raw.sourceFileDir === dir
+  })
+
+  return post ?? null
+}

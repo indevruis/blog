0d3003289259fd1fae63b09a237a3f1c9046ddbd
ruirun <indevruis@gmail.com>
2024-01-22 12:43:16 +0900

    Style: project list styling

--- src/containers/project/ProjectList.tsx
+++ src/containers/project/ProjectList.tsx
@@ -6,17 +6,19 @@ import { format, parseISO } from 'date-fns'
 const ProjectList = ({ post }: { post: Post }) => {
   return (
     <Link href={`/${post._raw.flattenedPath}`}>
-      <li className="border w-[310px] p-5 rounded-lg">
+      <li className="border w-[310px] p-5 rounded-lg h-full">
         <div className="w-full h-full flex flex-col justify-between">
-          <Image
-            src={post.thumbnail!}
-            alt="project image"
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: '100%', height: 'auto' }}
-            className="rounded"
-          />
+          <div className="h-full flex flex-col justify-center">
+            <Image
+              src={post.thumbnail!}
+              alt="project image"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: '100%', height: 'auto' }}
+              className="rounded"
+            />
+          </div>
           <div>
             <h1 className="mt-2 font-semibold text-[16px]">{post.title}</h1>
             <p className="leading-5 whitespace-pre-line text-[13px]">
